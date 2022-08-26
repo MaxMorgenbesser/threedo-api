fe0496b304d73395f6347815c6c72ec633b50895
Max Morgenbesser <maxwellmorgenbesser@gmail.com>
2022-08-26 10:50:26 -0400

    added post and get

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,11 @@
-
 import functiions from 'firebase-functions'
 import express from 'express'
 import cors from 'cors'
 import { getTasks, createTasks, updateTask, deleteTask } from './src/tasks'
 
-
-
 const app = express()
 app.use(cors())
 app.use(express.json())
-
 
 app.get('/tasks', getTasks)
 app.post('/tasks', createTasks)
@@ -17,14 +13,5 @@ app.patch('/tasks', updateTask)
 app.delete('/tasks', deleteTask)
 
 
-
-
-
 export const api = functiions.https.onRequest(app)
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
--- functions/src/tasks.js
+++ functions/src/tasks.js
@@ -1,18 +1,45 @@
-export const getTasks=(req,res)=>{
-    res.send('tasks')
-}
-export const createTasks=(req,res)=>{
-   const newTask = req.body
-   res.status(201).send("task added")
-}
-export const updateTask=(req,res)=>{
-   const taskUpdate=req.body
-   const {taskId} = req.params
-   res.status(202).send('task updated')
-}
-export const deleteTask=(req,res)=>{
-const {taskId}= req.params;
-res.status(203).send("task deleted")
-    
-}
+import DbConnect from "./DbConnect";
 
+export const getTasks = async (req, res) => {
+   // connect to database
+  const db = DbConnect();
+   //connect to collection and gets all
+ const collection = await db.collection("tasks").get()
+   //handle errors
+ .catch(err=>res.status(500).send(err))
+   // gets each item as an object returns the object with the id of the items
+ const tasks = collection.docs.map(doc =>{
+   let task = doc.data()
+   task.id = doc.id
+   return task
+ })
+  res.send("tasks");
+};
+
+
+export const createTasks = async (req, res) => {
+  const newTask = req.body;
+if (!newTask || !newTask.task){
+res.status(400).send({success:false, message:"Invalid Request"})
+return
+}
+const db = DbConnect()
+await db.collection("tasks").add(newTask)
+.catch(err=>res.status(500).send(err))
+res.status(201);
+getTasks(req,res)
+};
+
+
+
+export const updateTask = (req, res) => {
+  const taskUpdate = req.body;
+  const { taskId } = req.params;
+  res.status(202).send("task updated");
+};
+
+
+export const deleteTask = (req, res) => {
+  const { taskId } = req.params;
+  res.status(203).send("task deleted");
+};
